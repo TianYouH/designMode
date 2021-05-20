cb52431b115707e67d018c513699aff657e2a786
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-05-20 20:45:34 +0800

    feat: 配置babel环境

--- release/bundle.js
+++ release/bundle.js
@@ -7,6 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
@@ -15,7 +16,7 @@
   \**********************/
 /***/ (() => {
 
-eval("alert(\"你好世界\")\n\n//# sourceURL=webpack://designmode/./src/index.js?");
+eval("\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nalert(123);\n\nvar Person = function () {\n  function Person(name) {\n    _classCallCheck(this, Person);\n\n    this.name = name;\n  }\n\n  _createClass(Person, [{\n    key: \"say\",\n    value: function say() {\n      console.log(this.name);\n    }\n  }]);\n\n  return Person;\n}();\n\nvar p1 = new Person(\"黄金胡\");\n\np1.say();\n\n//# sourceURL=webpack://designmode/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("你好")
+
+alert(123)
+class Person {
+  constructor (name) {
+    this.name = name;
+  }
+  say() {
+    console.log(this.name);
+  }
+}
+
+const p1 = new Person("黄金胡");
+
+p1.say();
