a0d024d5149dacfa840e6a4640e207d2a395998c
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-05-21 21:03:04 +0800

    提交 面向对象测试

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-
-alert(123)
+// 类-模板
 class Person {
-  constructor (name) {
+  constructor (name, age) {
     this.name = name;
+    this.age = age;
   }
-  say() {
-    console.log(this.name);
+  eat() {
+    console.log(`${this.name} 吃 饭饭 `);
+  }
+  speak() {
+    console.log(`我叫 ${this.name}，今年 ${this.age} 岁`);
   }
 }
 
-const p1 = new Person("黄金胡");
+// 对象-实例
+const p1 = new Person("黄金胡", 18);
 
-p1.say();
+p1.eat();
+p1.speak();
+
+
+console.log(4545454);
